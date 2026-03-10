033900802e28fc2276c43f5fffae8e19ac40f1ca
Ilene_Dragon <keali.irene@gmail.com>
2026-03-10 14:23:40 -0700

    Boo function created

---
+++ boo.h
@@ -0,0 +1,5 @@
+#include <iostream>
+
+void boo() {
+    std::cout << "Hello from booo\n" << std::endl;
+}
--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include "foo.h"
+#include "boo.h"
 
 int main() {
     std::cout << "Hello World!" << std::endl;
     foo();
+    boo();
     return 0;
 }
