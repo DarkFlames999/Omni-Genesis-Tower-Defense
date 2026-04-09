9dafec1f3076d83f14fca8291f28b7a7de586bed
Konner Knoll <knoll@nic.edu>
2026-04-08 21:09:49 -0700

    added game skeleton

---
+++ game.h
@@ -0,0 +1,27 @@
+/**
+ * @file game.h
+ * @author Konner Knoll
+ * @brief Define game state class, which manages all the handlers
+ * @version 0.1
+ * @date 2026-04-08
+ * 
+ * @copyright Copyright (c) 2026
+ * 
+ */
+
+#ifndef GAME_H
+#define GAME_H
+
+class Game{
+public:
+    Game(){};
+    ~Game(){};
+    void activateHandlers();
+    void activateInputHandler();
+    void activateEntityHandler();
+private:
+    enum mState{menu, game, paused};
+    enum mDifficulty{none, easy, medium, hard};
+};
+
+#endif
